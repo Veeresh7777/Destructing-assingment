966683a2e9ee36fbd22f230c9bf4496eeb80ed79
Veeresh7777 <123634683+Veeresh7777@users.noreply.github.com>
2024-03-29 14:46:50 +0530

    Update wordcount.js

--- Assignment/wordcount.js
+++ Assignment/wordcount.js
@@ -1,1 +1,19 @@
+function wordcount(text) {
+    const words = text.split(/\s+/);
+    const count = {};
 
+    for(const el of words) {
+        if(el in count) {
+            count[el]++;
+        }
+        else
+        {
+            count[el]=1;
+        }
+    }
+
+    return count
+}
+
+const text = "pw skill is a amazing platform. pw skill also gives nice edu."
+console.log(wordcount(text));
